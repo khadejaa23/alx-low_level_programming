0674817dc1a193a740dee3d3ccf0957c3706cdd2
khadejaa23 <khadeja.ezzat23@gmail.com>
2022-12-31 16:56:13 -0800

    2-assembler

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -11,21 +11,11 @@
  *
  *     */
 
-=======
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
->>>>>>> 7c306a4268f26f3e73b03ccebd2c85483862f0ef
+
 int main(void)
 
 {
-<<<<<<< HEAD
 
 		return (0);
 
-
-=======
-    return (0);
->>>>>>> 7c306a4268f26f3e73b03ccebd2c85483862f0ef
 }
