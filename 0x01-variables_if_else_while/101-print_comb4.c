ddbd324c61a3d6fe5a690ff8cae5b4f9f8e7eef2
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-03 12:58:15 -0800

    i 101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,28 +8,28 @@
  */
 
 int main(void)
-
+	
 {
+	
+	int d, p, q;
+	
+	for (d = '0'; d < '9'; d++)
+	{
 
-         int d, p, q;
-  
-         for (d = '0'; d < '9'; d++)
-         {
+	for (p = d + 1; p < '9'; p++)
+	{
 
-         for (p = d + 1; p < '9'; p++)
-         {
-
-         for (q = d + 1; q < '9'; q++)
-         {
-         if (d == '7' && p == '8')
-         {
-         continue;
-         putchar(',');
-         putchar(' ');
-         }
-         }
-         }
-         }
-         putchar('\n');
-         return (0);
+	for (q = d + 1; q < '9'; q++)
+	{
+	if (d == '7' && p == '8')
+	{
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
