dde36ac896e050b58a32173ea881578a9ebcf32f
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-03 13:45:29 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,15 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 
 {
 
 	int p, q;
-	
+
 	for (p = 0; p <= 98; p++)
 	{
-	
+
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
