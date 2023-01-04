ec76adb01ed242c33d5f7ebc4c233f011a0f960d
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-03 19:02:31 -0800

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,12 +11,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
-	for (i = 0; s[i] != '\0'; n++)
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
-			return (s + i);
+				return (s + i);
 		}
 	}
 
