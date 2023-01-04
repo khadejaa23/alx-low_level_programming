e8c6908d95d7cf86231417ea8df9121d69cfb169
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-04 15:42:26 -0800

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,23 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, n, value, check;
 
 	value = 0;
-	
-	for (i = 0; accept[n] != '\0'; n++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (accept[n] == s[i])
+		check = 0;
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			value++;
-			check = 1;
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+
+		if (check == 0)
+		{
+			check++;
 		}
 	}
-
-	if (check == 0)
-	{
-		return (value);
-	}
+	return (i);
 }
