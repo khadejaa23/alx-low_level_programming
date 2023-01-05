741e0db0d401f82e7f8874d755a7df4a37d7d220
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-04 16:58:58 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * _strpbrk - Entry point
+ * set_string - Entry point
  * @s: input
- * @accept: input
+ * @to: input
  * Return: Always 0 (Success)
  */
 void set_string(char **s, char *to)
