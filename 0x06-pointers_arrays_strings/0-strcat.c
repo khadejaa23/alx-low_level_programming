9a00a0fc2931d26e3c03f45adfb05ec84c3e4c64
Khadiga Ezzat Abd AlQawy Yousef <47293700+khadejaa23@users.noreply.github.com>
2023-01-09 03:47:56 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminat
  *            null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer ro the string to string to be concatenated upon.
+ * @dest: The destination string
  * @src: The source string to be appended to be @dest.
  *
  * Return: A pointer to the destination string @dest.
