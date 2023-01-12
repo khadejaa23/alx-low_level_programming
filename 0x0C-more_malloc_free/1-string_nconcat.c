2009ff2c18b2ef543c3a6e66f5ccffc7a081ce61
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-12 14:02:16 -0800

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[size1] != '\0')
 	{
 		size1++;
@@ -26,10 +29,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2++;
 	}
 
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
 	if (p == NULL)
 		return (0);
 
 	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for  (; i < (size1 + n); i++)
 	{
 		p[i] = s2[i - size1];
 	}
