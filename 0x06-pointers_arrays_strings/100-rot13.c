14d23cb060f77d36c34ef38f32779a6acf368d1d
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-13 16:34:55 -0800

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,7 +2,7 @@
 
 /**
  * rot13 - Encodes a string using rot13.
- *
+ * @str: The string to be encoded.
  * Return: A pointer to the encoded string.
  */
 char *rot13(char *str)
@@ -37,7 +37,7 @@ char *rot13(char *str)
 			}
 
 		}
-		
+
 		indx1++;
 
 	}
