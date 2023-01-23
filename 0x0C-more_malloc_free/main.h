cd8ccfbe53e1706215d24865be01489f6ff80811
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-23 13:00:05 -0800

    delete file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,10 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_initialize_array(char *ar, int lar)
-void _is_zero(char *argv[])
-int main(int argc, char *argv[])
-int _checknum(char *argv[], int n)
+int main(int argc, char *argv[]);
 
 
 #endif
