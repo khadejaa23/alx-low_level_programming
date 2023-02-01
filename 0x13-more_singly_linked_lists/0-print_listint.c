02639934c58f3a17e5def3eac9956c6c658bbf3b
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-01-31 16:45:17 -0800

    listint_len

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_listint - Print all the elements of a listint_t list.
- * @n: A pointer to the head of the list_t list
+ * @h: A pointer to the head of the list_t list
  * 
  * Return: The number of nodes in the list_t list.
  */
