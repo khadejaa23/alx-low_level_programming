cb91aa77ae762cb8ee1847b3edac7861d16e8e69
khadejaa23 <khadeja.ezzat23@gmail.com>
2023-02-05 09:17:54 -0800

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,8 @@
  *	   Otherwise - 1.
  */
 
-int clear_bit(unsigned long int *n, unsigned int index)
+	int clear_bit(unsigned long int *n, unsigned int index)
+
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 
